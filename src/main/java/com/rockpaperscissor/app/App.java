2d36032d86ee2f9d07c98774321f6aab8560eb71
Andres Cordova <andres.cordova@telusinternational.com>
2023-02-20 16:42:16 -0600

    Model

--- src/main/java/com/rockpaperscissor/app/App.java
+++ src/main/java/com/rockpaperscissor/app/App.java
@@ -1,5 +1,7 @@
 package com.rockpaperscissor.app;
 
+import com.rockpaperscissor.app.Model.Shape;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        Shape shape = Shape.PAPER;
+        Shape shape2 = Shape.ROCK;
+        System.out.println(shape.winsWith(shape, shape2));
     }
 }
---
+++ src/main/java/com/rockpaperscissor/app/Model/Shape.java
@@ -0,0 +1,44 @@
+package com.rockpaperscissor.app.Model;
+
+public enum Shape {
+    ROCK,
+    SCISSOR,
+    PAPER;
+
+    public boolean winsWith(Shape shape1, Shape shape2){
+        if(shape1 == Shape.SCISSOR){
+            if (shape2 == Shape.ROCK) {
+                return false;
+            }
+            if (shape2 == Shape.PAPER) {
+                return true;
+            }
+            if (shape2 == Shape.SCISSOR) {
+                return false;
+            }
+        }
+        else if(shape1 == Shape.PAPER){
+            if (shape2 == Shape.ROCK) {
+                return true;
+            }
+            if (shape2 == Shape.SCISSOR) {
+                return false;
+            }
+            if (shape2 == Shape.PAPER) {
+                return false;
+            }
+        }
+        else if(shape1 == Shape.ROCK){
+            if (shape2 == Shape.ROCK) {
+                return false;
+            }
+            if (shape2 == Shape.SCISSOR) {
+                return true;
+            }
+            if (shape2 == Shape.PAPER) {
+                return false;
+            }
+        }
+        return false;
+    }
+}
