68d6b0d36cea73add59bac82f431286f1aec3a78
Oscar Recinos <oscar.recinos01@telusinternational.com>
2023-02-20 16:44:05 -0600

    adding shape enum

--- src/main/java/com/rockpaperscissor/app/App.java
+++ src/main/java/com/rockpaperscissor/app/App.java
@@ -1,5 +1,7 @@
 package com.rockpaperscissor.app;
 
+import com.rockpaperscissor.app.Model.Shape;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        Shape a = Shape.SCISSOR;
+
+       System.out.println(a.winsWith(a, Shape.PAPER)); 
+
+
     }
 }
