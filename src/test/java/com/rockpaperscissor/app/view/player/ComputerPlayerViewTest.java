8866877f52fa7e4ae45301647f9ebd405213b8e9
Oscar Recinos <oscar.recinos01@telusinternational.com>
2023-03-06 08:53:17 -0600

    Add mockito static extensions

--- src/test/java/com/rockpaperscissor/app/view/player/ComputerPlayerViewTest.java
+++ src/test/java/com/rockpaperscissor/app/view/player/ComputerPlayerViewTest.java
@@ -2,9 +2,13 @@ package com.rockpaperscissor.app.view.player;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 import com.rockpaperscissor.app.utils.Shape;
 
@@ -19,8 +23,10 @@ public class ComputerPlayerViewTest {
 
     @Test
     public void testGetName() {
-        String name = computerPlayerView.getName();
-        assertEquals(5, name.length());
+        MockedStatic<ComputerPlayerView> pc = Mockito.mockStatic(ComputerPlayerView.class);
+        pc.when( ()-> ComputerPlayerView.getRandomString(5)).thenReturn("ASDAS");
+        assertEquals(5 , ComputerPlayerView.getRandomString(5).length());
+
     }
 
     @Test
